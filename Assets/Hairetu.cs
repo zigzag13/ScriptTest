5b2a897ef49dc30ea7bf95b1a9f1bd4dd0a9299e
zigzag13 <dream_0gp5312m325239c@yahoo.co.jp>
2019-08-24 17:51:51 +0900

    Cスクリプトの課題
    配列とＭａｇｉｃ関数

---
+++ Assets/Hairetu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hairetu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {    
+        int[] array = new int[5];
+
+        array[0] = 30;
+        array[1] = 50;
+        array[2] = 70;
+        array[3] = 90;
+        array[4] = 110;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for(int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+		
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Boss
+{
+    private int mp = 53;
+
+    public void Magic()
+    {
+
+        if (mp >= 5)
+        {
+            this.mp -= 5;
+
+            Debug.Log("魔法攻撃をした。残りＭＰは" + mp);
+        }
+        else
+        {
+            Debug.Log("ＭＰが足りないため魔法が使えない。");
+        }
+    }
+
+}
+
+
+public class Test : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        Boss lastboss = new Boss();
+
+        for (int i = 0; i < 11; i++)
+        {
+            lastboss.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
